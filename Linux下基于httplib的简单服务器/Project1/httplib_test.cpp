1cf6e4ea33473e41951ef4d10951a28bf762a073
王一帆 <943550207@qq.com>
2019-08-01 10:54:50 +0800

    Add files via upload

---
+++ Linux下基于httplib的简单服务器/Project1/httplib_test.cpp
@@ -0,0 +1,17 @@
+﻿#include "httplib.h"
+using namespace httplib;
+
+void HelloWorld(const Request &req, Response &rsp)
+{
+	rsp.status = 302;//ÖØ¶¨Ïò
+	rsp.set_header("Location", "");
+	rsp.body = "<html><h1>Hello World</h4></html>";
+	return;
+}
+int main()
+{
+	Server server;
+	server.Get("/", HelloWorld);
+	server.listen("0.0.0.0", 9000);
+	return 0;
+}
